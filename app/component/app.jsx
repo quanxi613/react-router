4115c7dd63d269194729226ae2e71fe4824fcf0a
quanxi613 <quanxi613@126.com>
2016-08-24 15:05:40 +0800

    fix

--- app/component/app.jsx
+++ app/component/app.jsx
@@ -9,7 +9,7 @@ class App extends React.Component {
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About</Link></li>
-                    <li><Link to="/concat">Concat</Link></li>
+                    <li><Link to="/contact">Contact</Link></li>
                     <li><Link to="/list/1">List 1</Link></li>
                     <li><Link to="/list/2">List 2</Link></li>
                 </ul>
